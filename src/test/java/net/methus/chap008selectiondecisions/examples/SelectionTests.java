ddcc5e03d5d210393b3e38f63c28bef99e657d65
Methus <methus26@yahoo.com>
2017-09-18 00:46:32 +0300

    Selections and Decisions

--- src/test/java/net/methus/chap008selectiondecisions/examples/SelectionTests.java
+++ src/test/java/net/methus/chap008selectiondecisions/examples/SelectionTests.java
@@ -22,29 +22,90 @@ public class SelectionTests {
     }
 
     @Test
-    public void catOrCats(){
+    public void ifAddHttp() {
+        String url = "www.seleniumsimplified.com";
+        if(!url.startsWith("http")) {
+            url = addHttp(url);
+        }
 
-        int numberOfCats = 1;
-        assertEquals("1 == cat", "cat", (numberOfCats == 1) ? "cat" : "cats");
-
-        numberOfCats = 0;
-        assertEquals("0 == cats", "cats", (numberOfCats == 1) ? "cat" : "cats");
-
-        numberOfCats = 2;
-        assertEquals("2 == cats", "cats", (numberOfCats == 1) ? "cat" : "cats");
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
     }
 
     @Test
-    public void catsOrCatsAsMethod(){
-
-        assertEquals("1 == cat","cat", catOrCats(1));
-
-        assertEquals("0 == cats", "cats", catOrCats(0));
-
-        assertEquals("2 == cats", "cats", catOrCats(2));
+    public void ifElseAddHttp() {
+        String url = "www.seleniumsimplified.com";
+        if(url.startsWith("http")) {
+            // do nothing the url is fine
+        } else {
+            url = addHttp(url);
+        }
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
     }
 
-    private String catOrCats(int numberOfCats){
-        return (numberOfCats == 1) ? "cat" : "cats";
+    @Test
+    public void ifElseNestedAddHttp() {
+        String url = "seleniumsimplified.com";
+        if(url.startsWith("http")) {
+            // do nothing the url is fine
+        } else {
+            if(!url.startsWith("www")) {
+                url = "www." + url;
+            }
+            url = addHttp(url);
+        }
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
+    }
+
+    @Test
+    public void ifElseNestedAddHttpReformatted() {
+        String url = "seleniumsimplified.com";
+        if(url.startsWith("http"))
+        {
+            // do nothing the url is fine
+        } else
+        {
+            if(!url.startsWith("www"))
+            {
+                url = "www." + url;
+            }
+            url = addHttp(url);
+        }
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
+    }
+
+    @Test
+    public void switchExample() {
+        assertEquals("M", likelyGenderIs("sir"));
+        assertEquals("M", likelyGenderIs("mr"));
+        assertEquals("M", likelyGenderIs("master"));
+        assertEquals("F", likelyGenderIs("miss"));
+        assertEquals("F", likelyGenderIs("mrs"));
+        assertEquals("F", likelyGenderIs("ms"));
+        assertEquals("F", likelyGenderIs("lady"));
+        assertEquals("F", likelyGenderIs("madam"));
+    }
+
+    public String likelyGenderIs(String title) {
+        String likelyGender;
+
+        switch(title.toLowerCase()) {
+            case "sir":
+                likelyGender = "M";
+                break;
+            case "mr":
+                likelyGender = "M";
+                break;
+            case "master":
+                likelyGender = "M";
+                break;
+            default:
+                likelyGender = "F";
+                break;
+        }
+        return likelyGender;
     }
 }
